b5cf9ac365d5fc4363f522a603b3782f3e596301
Nick Tsourektsidis <creatorgr@hotmail.com>
2017-08-29 19:44:30 +0300

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/https://nicotsou.com/app-056c8feebbb02601e37e.js","e9f68d147166831859e0cadf2ee26504"],["/https://nicotsou.com/app-850e947a03089979dd58.js","e6cd992da0807f53438898effb8b0174"],["/https://nicotsou.com/app-981f27ecee743596f342.js","18f002c85a0419b51b8bd3215034484e"],["/https://nicotsou.com/app-c4911c12e4a19237c460.js","88e6da695c3379116d432be4cf7d458c"],["/https://nicotsou.com/commons-187bd840e708b4a86a1e.js","50e0a9de3a9461173838e89ea7ef10d2"],["/https://nicotsou.com/index.html","7e9f458bb5134a9bb5219e7f982c5931"],["/https://nicotsou.com/offline-plugin-app-shell-fallback/index.html","5c793a0f69867a8f0de0ff1c1dfc53ed"]];
+var precacheConfig = [["/nicotsou.com/app-056c8feebbb02601e37e.js","e9f68d147166831859e0cadf2ee26504"],["/nicotsou.com/app-7c15ce39878e7d991241.js","61fdc03841cd6913cce318bec46ebdb6"],["/nicotsou.com/app-850e947a03089979dd58.js","e6cd992da0807f53438898effb8b0174"],["/nicotsou.com/app-981f27ecee743596f342.js","18f002c85a0419b51b8bd3215034484e"],["/nicotsou.com/app-c4911c12e4a19237c460.js","f4cd24d47986692a6eba19d8d8f25e88"],["/nicotsou.com/commons-187bd840e708b4a86a1e.js","3bcf2fb8c8f58d1143d121e9bcd9f22a"],["/nicotsou.com/index.html","fc1c4ea9545d222e19ad087c00904963"],["/nicotsou.com/offline-plugin-app-shell-fallback/index.html","5f1f1872163c004979a58075d432933e"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
