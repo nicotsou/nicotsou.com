7f9af0aff36d407b2086e55315a13c27907ad33f
Nick Tsourektsidis <creatorgr@hotmail.com>
2018-10-27 00:04:50 +0200

    Add missing babel plugin

--- .babelrc.js
+++ .babelrc.js
@@ -5,5 +5,6 @@ const presetAbsPath = require(`path`).join(__dirname, 'babel-gatsby-preset.js')
 module.exports = {
   sourceMaps: true,
   presets: [presetAbsPath],
+  plugins: ['@babel/plugin-syntax-dynamic-import'],
   ignore,
 }
