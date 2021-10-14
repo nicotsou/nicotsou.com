695afc813b432ecae143b631b580443fdae196fd
Nicos Tsourektsidis <creatorgr@hotmail.com>
2021-10-14 08:10:10 +0200

    Rounded corners 👨‍🎨 (#17)
    * Add rounded corners in cover images
    
    * Add rounded corners post images

--- src/styles/BlogStyles.js
+++ src/styles/BlogStyles.js
@@ -125,6 +125,7 @@ const BlogStyles = css`
     @media screen and (min-width: 1200px) {
       max-width: calc(750px + 40%) !important;
       margin: 5rem auto 5rem -20% !important;
+      border-radius: calc(1.5 * 1rem);
     }
   }
 
@@ -142,6 +143,7 @@ const BlogStyles = css`
       @media screen and (min-width: 1200px) {
         max-width: calc(750px + 40%);
         margin: 5rem auto 5rem -20%;
+        border-radius: calc(1.5 * 1rem);
       }
     }
   }
