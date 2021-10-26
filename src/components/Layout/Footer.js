d416be5ce09964d1d87678e12b4c2dee6e9dff0c
Nicos Tsourektsidis <creatorgr@hotmail.com>
2021-10-26 20:55:29 +0200

    Update copyright date

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -95,7 +95,7 @@ const Footer = ({ className }) => {
           </a>
         </li>
       </SocialList>
-      <Copyright>&copy; 2020 - present NT</Copyright>
+      <Copyright>&copy; 2020-2 NT</Copyright>
     </StyledFooter>
   )
 }
